3edd95cff340174b13e8af44e8abe3b1285a30d2
kaliassistant <1325077@gmail.com>
2026-06-15 19:31:53 +0800

    fix inno setup docker priv problems

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM debian:testing
 
 RUN apt-get update \
- && apt-get install -y git make python3 python3-htmlmin python3-csscompressor python3-rjsmin ca-certificates wget curl mingw-w64 mingw-w64-tools mingw-w64-x86-64-dev docker.io\
+ && apt-get install -y --no-install-recommends git make python3 python3-htmlmin python3-csscompressor python3-rjsmin ca-certificates wget curl mingw-w64 mingw-w64-tools mingw-w64-x86-64-dev docker.io\
  && apt-get clean -y \
  && rm -rf /var/lib/apt/lists/*
 
--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 
 docker build --no-cache -t mp197mf-sk6812-webcli .
 
-docker run -it --rm -v $(pwd):/work mp197mf-sk6812-webcli make
+docker run --rm -i -v $(pwd):/work mp197mf-sk6812-webcli make
 
-docker run --rm -i -v $(pwd):/work amake/innosetup setup.iss
-
+docker container create --name setup amake/innosetup setup.iss
+docker cp ./hpevbsrv.exe setup:/work/
+docker cp ./setup.iss setup:/work/
+docker cp ./LICENSE.md setup:/work/
+docker cp ./icon.ico setup:/work/
+docker start -i -a setup
+mkdir -p ./Output
+docker cp setup:/work/Output/. ./Output/
+docker rm setup
